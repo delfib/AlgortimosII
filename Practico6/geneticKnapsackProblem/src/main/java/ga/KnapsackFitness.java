4cd8fdcdcfb2a059a4feb5c53a65dd721565b29e
Delfina Buil <delfinabuil2@gmail.com>
2023-05-16 12:40:45 -0300

    Practico 6 arreglado para la jar

--- Practico6/geneticKnapsackProblem/src/main/java/ga/KnapsackFitness.java
+++ Practico6/geneticKnapsackProblem/src/main/java/ga/KnapsackFitness.java
@@ -9,26 +9,26 @@ import org.jgap.IChromosome;
  */
 public class KnapsackFitness extends FitnessFunction {
 
-  /**
-   * reference to knapsack instance.
-   */
-  private KnapsackProblem myKnapsack;
+    /**
+     * reference to knapsack instance.
+     */
+    private KnapsackProblem myKnapsack;
 
-  /**
-   * KnapsackFitness constructor.
-   * @param knapsack instance.
-   */
-  public KnapsackFitness(KnapsackProblem knapsack) {
-    myKnapsack = knapsack;
-  }
+    /**
+    * KnapsackFitness constructor.
+    * @param knapsack instance.
+    */
+    public KnapsackFitness(KnapsackProblem knapsack) {
+        myKnapsack = knapsack;
+    }
 
-  /**
-   * Implements the fitness function, how good is the chromosome I am evaluating.
-   * @param chromosome Chromosome to evaluate
-   * @return fitness value
-   */
-  @Override
-  protected double evaluate(IChromosome chromosome) {
-    throw new UnsupportedOperationException("method not yet implemented");
-  }
+    /**
+    * Implements the fitness function, how good is the chromosome I am evaluating.
+    * @param chromosome Chromosome to evaluate
+    * @return fitness value
+    */
+    @Override
+    protected double evaluate(IChromosome chromosome) {
+        throw new UnsupportedOperationException("method not yet implemented");
+    }
 }
--- Practico6/geneticKnapsackProblem/src/main/java/ga/KnapsackProblemGA.java
+++ Practico6/geneticKnapsackProblem/src/main/java/ga/KnapsackProblemGA.java
@@ -12,15 +12,15 @@ public class KnapsackProblemGA {
 	/**
 	 * Main method.
 	 * Create a Configuration and Initial Genotype,
-   * with the population size of configuration for JGAP Library.
+   	 * with the population size of configuration for JGAP Library.
 	 * Evolve the problem.
 	 * @param args Arguments for the main function.
 	 */
 	public static void main(String[] args) throws InvalidConfigurationException {
-    Configuration conf = new DefaultConfiguration();
-    KnapsackProblem myKnapsack = new KnapsackProblem(50);
-    KnapsackFitness myFunc = new KnapsackFitness(myKnapsack);
-    //TODO: Create your own Configuration and Initial Genotype
-    //TODO: Evolve the population.
+		Configuration conf = new DefaultConfiguration();
+		KnapsackProblem myKnapsack = new KnapsackProblem(50);
+		KnapsackFitness myFunc = new KnapsackFitness(myKnapsack);
+		//TODO: Create your own Configuration and Initial Genotype
+		//TODO: Evolve the population.
 	}
 }
